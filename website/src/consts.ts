8c70fab39febfb639e2b4621a20c7ffb934fdcbe
Peter Pivarnik <peter.pivarnik@gmail.com>
2026-05-21 22:47:44 +0200

    feat: Initialize Astro website project

---
+++ website/src/consts.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'Špacírkovník';
+export const SITE_DESCRIPTION = 'Rodinné prechádzky, ktoré sa hrajú ako dobrodružstvo. Sleduj mapu, lúšti hádanky, objavuj mesto!';
