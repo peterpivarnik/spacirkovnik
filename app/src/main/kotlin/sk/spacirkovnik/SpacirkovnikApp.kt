d4fab7a3847ae66f72ed303a823b74fd6cb5d4e8
Peter Pivarnik <peter.pivarnik@gmail.com>
2026-05-29 16:27:27 +0200

    feat: Add custom Application class to disable Mapbox telemetry

---
+++ app/src/main/kotlin/sk/spacirkovnik/SpacirkovnikApp.kt
@@ -0,0 +1,20 @@
+package sk.spacirkovnik
+
+import android.annotation.SuppressLint
+import android.app.Application
+import com.mapbox.common.TelemetryUtils
+
+class SpacirkovnikApp : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        disableMapboxTelemetry()
+    }
+
+    @SuppressLint("RestrictedApi")
+    // TelemetryUtils.setEventsCollectionState je označené @RestrictTo(LIBRARY_GROUP_PREFIX),
+    // no ide o jediné dostupné API na vypnutie zberu dát v Mapbox SDK v11.
+    // Verejné alternatívy (MapboxOptions, SettingsService) túto možnosť neponúkajú.
+    private fun disableMapboxTelemetry() {
+        TelemetryUtils.setEventsCollectionState(false) { /* ignorujeme výsledok */ }
+    }
+}
